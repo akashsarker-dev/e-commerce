522c72681a080652e3134a1793c61e9cf027af8a
akashsarker24 <akashsarkeronline@gmail.com>
2024-02-02 22:05:08 +0600

    footer style

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,47 +1,55 @@
 import React from "react";
 import logo from "../../assets/Logo.png";
+import { FaFacebookF } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="max-w-container mx-auto flex flex-wrap justify-between">
-      <div className="w-2/5 flex justify-between">
+    <div className="max-w-container mx-auto flex flex-wrap justify-between py-16">
+      
+      <div className=" w-2/5 flex ">
         <div>
-          <p>MENU</p>
-          <ul>
-            <li>Home</li>
-            <li>Shop</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </div>
 
-        <div>
-          <p>MENU</p>
-          <ul>
-            <li>Home</li>
-            <li>Shop</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </div>
-
-        <div>
-          <p>MENU</p>
-          <ul>
-            <li>Home</li>
-            <li>Shop</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
+          <p className=" text-xl text-primary font-medium w-[320px] py-7">We are a team of designers and
+developers that create high quality
+WordPress</p>
+<div className='flex gap-3'>
+        <FaFacebookF></FaFacebookF>
+        <FaLinkedinIn></FaLinkedinIn>
+        <FaInstagram></FaInstagram>
+    </div>
         </div>
       </div>
-      <div className="w-3/5 flex ">
+      <div className="w-3/5 flex justify-between">
         <div>
-          <p>(052) 611-5711 company@domain.com</p>
-          <p>575 Crescent Ave. Quakertown, PA 18951</p>
+          <p>MENU</p>
+          <ul>
+            <li>Home</li>
+            <li>Shop</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
         </div>
+
         <div>
-          <img src={logo} alt="" srcset="" />
+          <p>MENU</p>
+          <ul>
+            <li>Home</li>
+            <li>Shop</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+
+        <div>
+          <p>MENU</p>
+          <ul>
+            <li>Home</li>
+            <li>Shop</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
         </div>
       </div>
     </div>
