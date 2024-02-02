a451ddc0e3fe28210f7bd64cf770cec5c7fcec98
akashsarker24 <akashsarkeronline@gmail.com>
2024-02-02 21:55:29 +0600

    product part

--- src/pages/newproduct/Newproduct.jsx
+++ src/pages/newproduct/Newproduct.jsx
@@ -6,35 +6,252 @@ import { FaShoppingCart } from "react-icons/fa";
 
 const Newproduct = () => {
   return (
-    <div className="container mx-auto py-28">
-      <h3 className="mb-12">New Arrivals</h3>
-       <div>
-       <div className="w-[370px] ">
+    <div className="max-w-container mx-auto py-28">
+      <h3 className="mb-12 text-4xl font-roboto font-bold">New Arrivals</h3>
+      <div>
+        <div className="grid grid-cols-4 gap-4">
+          <div className="w-[300px] border-2 border-gray-300">
             <div className=" group relative ">
-          <img src={ProductImg} alt="" />
-            <div className=" absolute bottom-0 left-0 flex flex-col  float-right justify-center h-[156px] w-full group-hover:bg-[#00000061] opacity-0 group-hover:opacity-100 duration-300 ">
-             <div className=" flex gap-3 items-center justify-end ">
-             <p>Add to Wish List</p>
-              <IoIosHeart />
-             </div>
+              <img src={ProductImg} alt="" />
+              {/* <div className=" absolute bottom-0 left-0 flex flex-col  float-right justify-center h-[156px] w-full group-hover:bg-[#00000061] opacity-0 group-hover:opacity-100 duration-300 ">
               <div className=" flex gap-3 items-center justify-end ">
-              <p>Compare</p>
-              <HiOutlineRefresh />
+                <p>Add to Wish List</p>
+                <IoIosHeart />
               </div>
-             <div className=" flex gap-3 items-center justify-end ">
-             <p>Add to Cart</p>
-              <FaShoppingCart />
-             </div>
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Compare</p>
+                <HiOutlineRefresh />
+              </div>
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Add to Cart</p>
+                <FaShoppingCart />
+              </div>
+            </div> */}
+            </div>
+            <div className=" py-5 pl-4 border-t-2 border-b-slate-300">
+              <p className=" text-third-primary font-roboto font-medium">
+                Electronics
+              </p>
+              <h4 className="text-lg font-medium text-primary w-[300px] py-2 ">
+                Mini Portable Mobile Phone Powerbank for
+              </h4>
+              <p className="text-[#0989FF] font-roboto font-bold text-base">
+                125.00$
+              </p>
+            </div>
+          </div>
+          <div className="w-[300px] border-2 border-gray-300">
+            <div className=" group relative ">
+              <img src={ProductImg} alt="" />
+              {/* <div className=" absolute bottom-0 left-0 flex flex-col  float-right justify-center h-[156px] w-full group-hover:bg-[#00000061] opacity-0 group-hover:opacity-100 duration-300 ">
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Add to Wish List</p>
+                <IoIosHeart />
+              </div>
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Compare</p>
+                <HiOutlineRefresh />
+              </div>
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Add to Cart</p>
+                <FaShoppingCart />
+              </div>
+            </div> */}
+            </div>
+            <div className=" py-5 pl-4 border-t-2 border-b-slate-300">
+              <p className=" text-third-primary font-roboto font-medium">
+                Electronics
+              </p>
+              <h4 className="text-lg font-medium text-primary w-[300px] py-2 ">
+                Mini Portable Mobile Phone Powerbank for
+              </h4>
+              <p className="text-[#0989FF] font-roboto font-bold text-base">
+                125.00$
+              </p>
+            </div>
+          </div>
+          <div className="w-[300px] border-2 border-gray-300">
+            <div className=" group relative ">
+              <img src={ProductImg} alt="" />
+              {/* <div className=" absolute bottom-0 left-0 flex flex-col  float-right justify-center h-[156px] w-full group-hover:bg-[#00000061] opacity-0 group-hover:opacity-100 duration-300 ">
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Add to Wish List</p>
+                <IoIosHeart />
+              </div>
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Compare</p>
+                <HiOutlineRefresh />
+              </div>
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Add to Cart</p>
+                <FaShoppingCart />
+              </div>
+            </div> */}
+            </div>
+            <div className=" py-5 pl-4 border-t-2 border-b-slate-300">
+              <p className=" text-third-primary font-roboto font-medium">
+                Electronics
+              </p>
+              <h4 className="text-lg font-medium text-primary w-[300px] py-2 ">
+                Mini Portable Mobile Phone Powerbank for
+              </h4>
+              <p className="text-[#0989FF] font-roboto font-bold text-base">
+                125.00$
+              </p>
+            </div>
+          </div>
+          <div className="w-[300px] border-2 border-gray-300">
+            <div className=" group relative ">
+              <img src={ProductImg} alt="" />
+              {/* <div className=" absolute bottom-0 left-0 flex flex-col  float-right justify-center h-[156px] w-full group-hover:bg-[#00000061] opacity-0 group-hover:opacity-100 duration-300 ">
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Add to Wish List</p>
+                <IoIosHeart />
+              </div>
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Compare</p>
+                <HiOutlineRefresh />
+              </div>
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Add to Cart</p>
+                <FaShoppingCart />
+              </div>
+            </div> */}
+            </div>
+            <div className=" py-5 pl-4 border-t-2 border-b-slate-300">
+              <p className=" text-third-primary font-roboto font-medium">
+                Electronics
+              </p>
+              <h4 className="text-lg font-medium text-primary w-[300px] py-2 ">
+                Mini Portable Mobile Phone Powerbank for
+              </h4>
+              <p className="text-[#0989FF] font-roboto font-bold text-base">
+                125.00$
+              </p>
+            </div>
+          </div>
+          <div className="w-[300px] border-2 border-gray-300">
+            <div className=" group relative ">
+              <img src={ProductImg} alt="" />
+              {/* <div className=" absolute bottom-0 left-0 flex flex-col  float-right justify-center h-[156px] w-full group-hover:bg-[#00000061] opacity-0 group-hover:opacity-100 duration-300 ">
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Add to Wish List</p>
+                <IoIosHeart />
+              </div>
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Compare</p>
+                <HiOutlineRefresh />
+              </div>
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Add to Cart</p>
+                <FaShoppingCart />
+              </div>
+            </div> */}
+            </div>
+            <div className=" py-5 pl-4 border-t-2 border-b-slate-300">
+              <p className=" text-third-primary font-roboto font-medium">
+                Electronics
+              </p>
+              <h4 className="text-lg font-medium text-primary w-[300px] py-2 ">
+                Mini Portable Mobile Phone Powerbank for
+              </h4>
+              <p className="text-[#0989FF] font-roboto font-bold text-base">
+                125.00$
+              </p>
+            </div>
+          </div>
+          <div className="w-[300px] border-2 border-gray-300">
+            <div className=" group relative ">
+              <img src={ProductImg} alt="" />
+              {/* <div className=" absolute bottom-0 left-0 flex flex-col  float-right justify-center h-[156px] w-full group-hover:bg-[#00000061] opacity-0 group-hover:opacity-100 duration-300 ">
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Add to Wish List</p>
+                <IoIosHeart />
+              </div>
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Compare</p>
+                <HiOutlineRefresh />
+              </div>
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Add to Cart</p>
+                <FaShoppingCart />
+              </div>
+            </div> */}
+            </div>
+            <div className=" py-5 pl-4 border-t-2 border-b-slate-300">
+              <p className=" text-third-primary font-roboto font-medium">
+                Electronics
+              </p>
+              <h4 className="text-lg font-medium text-primary w-[300px] py-2 ">
+                Mini Portable Mobile Phone Powerbank for
+              </h4>
+              <p className="text-[#0989FF] font-roboto font-bold text-base">
+                125.00$
+              </p>
+            </div>
+          </div>
+          <div className="w-[300px] border-2 border-gray-300">
+            <div className=" group relative ">
+              <img src={ProductImg} alt="" />
+              {/* <div className=" absolute bottom-0 left-0 flex flex-col  float-right justify-center h-[156px] w-full group-hover:bg-[#00000061] opacity-0 group-hover:opacity-100 duration-300 ">
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Add to Wish List</p>
+                <IoIosHeart />
+              </div>
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Compare</p>
+                <HiOutlineRefresh />
+              </div>
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Add to Cart</p>
+                <FaShoppingCart />
+              </div>
+            </div> */}
+            </div>
+            <div className=" py-5 pl-4 border-t-2 border-b-slate-300">
+              <p className=" text-third-primary font-roboto font-medium">
+                Electronics
+              </p>
+              <h4 className="text-lg font-medium text-primary w-[300px] py-2 ">
+                Mini Portable Mobile Phone Powerbank for
+              </h4>
+              <p className="text-[#0989FF] font-roboto font-bold text-base">
+                125.00$
+              </p>
+            </div>
+          </div>
+          <div className="w-[300px] border-2 border-gray-300">
+            <div className=" group relative ">
+              <img src={ProductImg} alt="" />
+              {/* <div className=" absolute bottom-0 left-0 flex flex-col  float-right justify-center h-[156px] w-full group-hover:bg-[#00000061] opacity-0 group-hover:opacity-100 duration-300 ">
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Add to Wish List</p>
+                <IoIosHeart />
+              </div>
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Compare</p>
+                <HiOutlineRefresh />
+              </div>
+              <div className=" flex gap-3 items-center justify-end ">
+                <p>Add to Cart</p>
+                <FaShoppingCart />
+              </div>
+            </div> */}
+            </div>
+            <div className=" py-5 pl-4 border-t-2 border-b-slate-300">
+              <p className=" text-third-primary font-roboto font-medium">
+                Electronics
+              </p>
+              <h4 className="text-lg font-medium text-primary w-[300px] py-2 ">
+                Mini Portable Mobile Phone Powerbank for
+              </h4>
+              <p className="text-[#0989FF] font-roboto font-bold text-base">
+                125.00$
+              </p>
+            </div>
+          </div>
         </div>
-            </div>
-            <div className="flex justify-between">
-              <h4 className="text-lg font-semibold">Basic Crew Neck Tee</h4>
-              <p className="text-gray-600">$44.00</p>
-            </div>
-            <p className="text-gray-600">Black</p>
-      
       </div>
-       </div>
     </div>
   );
 };
