8a9c3af0018d64c592faf13d171f0d596fa7732e
akashsarker24 <akashsarkeronline@gmail.com>
2024-03-01 21:39:00 +0600

    slick  slider:

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,12 +1,58 @@
-import React from 'react'
-import BannerImg from '../../assets/banner.png'
+import React from "react";
+import Slider from "react-slick";
+import BannerImg from "../../assets/banner.png"
+import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 
-const Banner = () => {
+function Banner() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    appendDots: dots => (
+      <div
+        style={{
+          borderRadius: "10px",
+          padding: "10px"
+        }}
+      >
+        <ul style={{ margin: "0px",
+         display:"block",
+         position:'absolute',
+         top:"50%",
+         transform:"translateY(-50%)",
+         left:"180px"
+       }}> {dots} </ul>
+      </div>
+    ),
+    customPaging: i => (
+      <div
+        style={{
+          width: "30px",
+          color: "blue",
+          borderRight:"2px solid red"
+        }}>
+        {i + 1}
+      </div>
+    )
+  };
   return (
-    <div className=' max-w-container mx-auto'>
-    <img src={BannerImg} alt="" srcset="" />
+    <div className="slider-container mx-auto">
+      <Slider {...settings}>
+        <div>
+          <img className=" w-full" src={BannerImg} alt=""  />
+        </div>
+        <div>
+          <img className=" w-full" src={BannerImg} alt=""  />
+        </div>
+        <div>
+          <img className=" w-full" src={BannerImg} alt=""  />
+        </div>
+        
+      </Slider>
     </div>
-  )
+  );
 }
-
-export default Banner
+export default Banner;
